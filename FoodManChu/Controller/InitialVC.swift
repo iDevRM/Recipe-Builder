49e6e02d0e4276988bd72fe357a970cbed4e6be7
Rick <rmartinez1611@gmail.com>
2021-01-08 15:51:56 -0500

    setting up view controllers and UI

--- FoodManChu/Controller/InitialVC.swift
+++ FoodManChu/Controller/InitialVC.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class InitialVC: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func button1WasTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "searchVCSegue", sender: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func button2WasTapped(_ sender: UIButton) {
+        
     }
-    */
+    
+    @IBAction func button3WasTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func button4WasTapped(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func button5WasTapped(_ sender: UIButton) {
+        
+    }
 
 }
---
+++ FoodManChu/Controller/SearchVC.swift
@@ -0,0 +1,22 @@
+//
+//  SearchVC.swift
+//  FoodManChu
+//
+//  Created by Ricardo Martinez on 1/8/21.
+//
+
+import UIKit
+import CoreData
+
+class SearchVC: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
