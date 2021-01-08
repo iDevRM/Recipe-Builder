bd3eb5f5011ed559421c1d91ebde62b58784afa4
Rick <rmartinez1611@gmail.com>
2021-01-08 17:52:36 -0500

    setting up cell configuration

--- FoodManChu/Controller/SearchVC.swift
+++ FoodManChu/Controller/SearchVC.swift
@@ -15,8 +15,30 @@ class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         
     }
 
+}
+
+//MARK: - TableView Delegate and DataSource
+
+extension SearchVC: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    
+    
 }
--- FoodManChu/View/SearchTableViewCell.swift
+++ FoodManChu/View/SearchTableViewCell.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnail:   UIImageView!
+    @IBOutlet weak var topLabel:    UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureCell(_ recipe: Recipe) {
+        self.thumbnail = 
     }
 
 }
