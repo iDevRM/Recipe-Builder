52149fd4558e5a185f787290e7d27fa1b699fedc
Rick <rmartinez1611@gmail.com>
2021-01-19 17:34:08 -0500

    finished search feature in app

--- FoodManChu/Controller/SearchVC.swift
+++ FoodManChu/Controller/SearchVC.swift
@@ -12,20 +12,41 @@ class SearchVC: UIViewController, NSFetchedResultsControllerDelegate {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     
     var recipeArray = [Recipe]()
-    
+    var ingredients = Ingredients()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadRecipes()
         
+        loadRecipes()
+                
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
         
-        
-        print(recipeArray.count)
     }
+    
+    @IBAction func segmentControlTapped(_ sender: UISegmentedControl) {
+        
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("new phone who dis")
+        case 1:
+            print("new phone who dis")
+        case 2:
+            print("new phone who dis")
+        case 3:
+            print("new phone who dis")
+        case 4:
+            print("new phone who dis")
+        default:
+            break
+        }
+    }
+    
 
 }
 
@@ -50,15 +71,16 @@ extension SearchVC: UITableViewDelegate,UITableViewDataSource {
     
 }
 
-//MARK: - Core Data C.R.U.D functions
+//MARK: - Core Data Manipulation Methods
 extension SearchVC {
     
     func addNewRecipe() {
         let newItem = Recipe(context: Constants.context)
-        newItem.name = "Mealoaf"
-        newItem.descript = "Delicious meatloaf, said no one ever."
-        newItem.instructions = "Just cook it idiot."
-        newItem.prepTime = 30
+        newItem.name = "Chicken Parmasean"
+        newItem.descript = "Crispy and tender chicken with hot marinara sauce and fresh parmasean."
+        newItem.instructions = "Its amazing just try it"
+        newItem.prepTime = 35
+      
         
         saveItems()
     }
@@ -81,6 +103,45 @@ extension SearchVC {
             print("Error fetching data from context: \(error)")
         }
         tableView.reloadData()
+    }
+    
+    func removeRecipe(at index: Int) {
+        Constants.context.delete(recipeArray[index])
+        recipeArray.remove(at: index)
+        saveItems()
+    }
+    
+}
+
+//MARK: - Core Data Search Delegates
+
+extension SearchVC: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+
+        let predicate = NSPredicate(format: "descript CONTAINS[cd] %@", searchBar.text!)
+
+        request.predicate = predicate
+
+        do {
+            recipeArray = try Constants.context.fetch(request)
+        } catch {
+            print("Error fetching data from context: \(error)")
+        }
+
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadRecipes()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
     
 }
