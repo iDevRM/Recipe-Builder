7f5e44f48e7f0c29d72613a0d4826ce34a1280e2
Rick <rmartinez1611@gmail.com>
2021-01-20 16:17:49 -0500

    setting up addVC

---
+++ FoodManChu/Controller/AddVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddVC.swift
+//  FoodManChu
+//
+//  Created by Ricardo Martinez on 1/20/21.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
